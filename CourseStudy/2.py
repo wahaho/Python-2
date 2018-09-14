8b9b5e62451d67ad9bed0b04130977f42ca968e6
wahaho <153994159@qq.com>
2018-09-14 14:43:11 +0800

    tiaozheng

--- CourseStudy/2.py
+++ CourseStudy/2.py
@@ -1,10 +1,10 @@
 #coding=utf-8
-name = "xiaoWei"
+name = "xiao"
 age = "18"
 adr = "山东"
 print("==========")
 print(name+adr+str(age))
 
-print("名字：\nxiaoWei")
+print("名字：\nxiao")
 print("联系方式：131XXXXXXX")
 print("地址：北京市")
